9dc77c748116c06842a38ae9894358da1e67f8c6
DeazardMuhammad <85541780+DeazardMuhammad@users.noreply.github.com>
2021-06-09 19:28:51 +0700

    Add files via upload

---
+++ StudiKasus1/src/studikasus1/StudiKasus1.java
@@ -0,0 +1,14 @@
+package studikasus1;
+
+public class StudiKasus1 {
+
+    public static void main(String[] args) {
+        
+        Tmpt Brng1 = new Tmpt ("Sapi","20.000.000");
+        Tmpt Brng2 = new Tmpt ("Kambing","3.000.000");     
+        Brng1.Halo();
+        Brng2.Halo();
+                
+    }
+    
+}
---
+++ StudiKasus1/src/studikasus1/Tmpt.java
@@ -0,0 +1,19 @@
+package studikasus1;
+
+public class Tmpt {
+
+    public String Hewan = new String();
+    public String Jumlah = new String();
+    
+    Tmpt(String Hewan, String Jumlah ){
+        this.Hewan = Hewan;
+        this.Jumlah = Jumlah;
+        
+    }
+    public void Halo(){
+        System.out.println("Hewan: "+ this.Hewan);
+        System.out.println("Jumlah: "+ this.Jumlah);
+    }
+    
+            
+}
